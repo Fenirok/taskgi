b7a56e7dd6f08383d76c5694cc9d9aacd6e88c82
Fenirok <codetode1001@gmail.com>
2026-04-13 14:13:44 +0530

    auth repository code

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,17 @@
+import '../entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<UserEntity> signUp({
+    required String email,
+    required String password,
+  });
+
+  Future<UserEntity> login({
+    required String email,
+    required String password,
+  });
+
+  Future<void> logout();
+
+  Stream<UserEntity?> get authStateChanges;
+}
